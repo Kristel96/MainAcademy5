d44fcf40221303dbdddab186f250071e7c7ab230
Khrystyna Rymar <>
2020-07-09 21:45:48 +0300

    js if

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = 11;
+let b = 16;
+
+let d=34;
+let c = 98;
+
+if (    (b===a || d<c) && (b<c && a<c)      ){
+    alert("hello");
+} else if(b===c){
+
+}else {
+    alert("TOOOO BAAAADDDD");
+}
+
+
+//(b===a)?alert("hello"):alert("bye bye");

0ff501e58497eacb50e02608e8b3ff3d4dd7dd19
khrystynarymar <rimar1996@gmail.com>
2020-07-23 19:31:48 +0300

    completed array tasks

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
-let a = 11;
-let b = 16;
+let myColor = ["Red", "Green", "White", "Black"];
+let allString = myColor.toString();
+console.log("1st task: "+allString);
 
-let d=34;
-let c = 98;
+let myColor2 = "qwqwqw";
+let checkresult = Array.isArray(myColor2);
+console.log("2st task: "+ checkresult);
 
-if (    (b===a || d<c) && (b<c && a<c)      ){
-    alert("hello");
-} else if(b===c){
+let emptyArr = [];
+emptyArr.push("Hello");
+emptyArr.push("World");
+emptyArr.unshift("Hi there!");
+console.log("3st task: ");
+console.dir(emptyArr);
 
-}else {
-    alert("TOOOO BAAAADDDD");
+let arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+let arr2 = arr1.sort();
+console.log("4st task: ");
+console.dir(arr2);
+
+
+
+let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+//V1
+let newArray= color.map(concatString);
+
+function concatString(value, index) {
+    let num = index+1;
+    return num +" choice is "+value;
+ }
+console.dir(newArray);
+
+//v2
+
+let newArr2 = [];
+
+for( let i =0; i<color.length;i++){
+    newArr2.push(i+1 + " choise is " + color[i])
 }
+console.dir(newArr2);
 
 
-//(b===a)?alert("hello"):alert("bye bye");
+

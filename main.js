6d8f80a30a4a6c6debb29cdeb5074034594f19e9
khrystynarymar <rimar1996@gmail.com>
2020-07-23 21:39:13 +0300

    added some logic for creating new elements in the DOM

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
-let myColor = ["Red", "Green", "White", "Black"];
-let allString = myColor.toString();
-console.log("1st task: "+allString);
+let imgNode=  document.createElement("img");
+imgNode.src = "assets/nature.jpg";
+imgNode.style.width = "400px";
 
-let myColor2 = "qwqwqw";
-let checkresult = Array.isArray(myColor2);
-console.log("2st task: "+ checkresult);
-
-let emptyArr = [];
-emptyArr.push("Hello");
-emptyArr.push("World");
-emptyArr.unshift("Hi there!");
-console.log("3st task: ");
-console.dir(emptyArr);
-
-let arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
-let arr2 = arr1.sort();
-console.log("4st task: ");
-console.dir(arr2);
+let mainCont = document.getElementById("mainContainer");
+mainCont.appendChild(imgNode);
+let article = document.createElement("p");
+let articleText = document.createTextNode("Appropriately monetize premium solutions through 2.0 networks. Collaboratively exploit corporate applications before compelling methodologies. Assertively productize value-added methodologies whereas viral architectures. Assertively disintermediate alternative products whereas process-centric processes. Objectively whiteboard market positioning experiences before dynamic channels.\n" +
+    "\n" +
+    "Globally pontificate out-of-the-box applications vis-a-vis emerging convergence. Efficiently customize high-quality niche markets through granular potentialities. Synergistically simplify e-business growth strategies after timely strategic theme areas. Compellingly seize resource-leveling metrics before seamless potentialities. Assertively administrate future-proof mindshare with ubiquitous customer service.\n" +
+    "\n" +
+    "Enthusiastically drive professional experiences via effective models. Energistically envisioneer leading-edge intellectual capital whereas transparent.");
+article.appendChild(articleText);
+mainCont.appendChild(article);
+let myButton = document.createElement("button");
+myButton.innerHTML = "HIDE!";
 
 
-
-let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
-//V1
-let newArray= color.map(concatString);
-
-function concatString(value, index) {
-    let num = index+1;
-    return num +" choice is "+value;
- }
-console.dir(newArray);
-
-//v2
-
-let newArr2 = [];
-
-for( let i =0; i<color.length;i++){
-    newArr2.push(i+1 + " choise is " + color[i])
-}
-console.dir(newArr2);
+mainCont.appendChild(myButton);
+console.log(mainCont);
+alert("wow");
+myButton.style.padding = "20px";
+myButton.style.border = "0";
 
 
+myButton.onclick = function(){
+    article.style.display = "none";
+};
 
+let myButton2 = document.createElement("button");
+myButton2.innerHTML = "SHOW!";
+myButton2.style.padding = "20px";
+myButton2.style.border = "0";
+
+mainCont.appendChild(myButton2);
+
+myButton2.onclick = function(){
+    article.style.display = "block";
+};

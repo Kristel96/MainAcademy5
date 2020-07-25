85138abe8f711b070eee1e931b9ad403927052f6
khrystynarymar <rimar1996@gmail.com>
2020-07-25 12:35:04 +0300

    created a regexp for email validation

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-let imgNode=  document.createElement("img");
-imgNode.src = "assets/nature.jpg";
-imgNode.style.width = "400px";
-
-let mainCont = document.getElementById("mainContainer");
-mainCont.appendChild(imgNode);
-let article = document.createElement("p");
-let articleText = document.createTextNode("Appropriately monetize premium solutions through 2.0 networks. Collaboratively exploit corporate applications before compelling methodologies. Assertively productize value-added methodologies whereas viral architectures. Assertively disintermediate alternative products whereas process-centric processes. Objectively whiteboard market positioning experiences before dynamic channels.\n" +
-    "\n" +
-    "Globally pontificate out-of-the-box applications vis-a-vis emerging convergence. Efficiently customize high-quality niche markets through granular potentialities. Synergistically simplify e-business growth strategies after timely strategic theme areas. Compellingly seize resource-leveling metrics before seamless potentialities. Assertively administrate future-proof mindshare with ubiquitous customer service.\n" +
-    "\n" +
-    "Enthusiastically drive professional experiences via effective models. Energistically envisioneer leading-edge intellectual capital whereas transparent.");
-article.appendChild(articleText);
-mainCont.appendChild(article);
-let myButton = document.createElement("button");
-myButton.innerHTML = "HIDE!";
+let submButton = document.getElementById("submitRegistration");
+submButton.onclick = function () {
+    // Validation logic will be here
+    let emailAdd = document.getElementById("email");
+    validateEmail(emailAdd);
 
 
-mainCont.appendChild(myButton);
-console.log(mainCont);
-alert("wow");
-myButton.style.padding = "20px";
-myButton.style.border = "0";
+    };
 
-
-myButton.onclick = function(){
-    article.style.display = "none";
-};
-
-let myButton2 = document.createElement("button");
-myButton2.innerHTML = "SHOW!";
-myButton2.style.padding = "20px";
-myButton2.style.border = "0";
-
-mainCont.appendChild(myButton2);
-
-myButton2.onclick = function(){
-    article.style.display = "block";
-};
+function validateEmail(email) {
+    console.dir(email);
+  let  emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  let result = emailPattern.test(email.value);
+  console.log(result);
+  if( result ===true){
+      email.style.border = "2px solid green"
+  }else{
+      email.style.border = "2px solid red"
+  }
+}

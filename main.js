78fca5317b5d1d748df7ee69f1525ca817873ea4
khrystynarymar <rimar1996@gmail.com>
2020-10-01 21:33:09 +0300

    added tabs and search

--- main.js
+++ main.js
@@ -1,68 +1,142 @@
 let apikey = "3bcb757b3ebe4adeeb1c3a7d2a3c4dcc";
-let language = "en";
+let language = "en-US";
 let page = 1;
-let url =`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}&language=${language}&page=${page}`;
 let imageServer = "https://image.tmdb.org/t/p/w300/";
-
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      let response = JSON.parse(xhttp.response);
-     let filmsArray =  response.results;
-     filmsArray.forEach(function (item) {
-         let container = getE("main");
+let urlBase = "https://api.themoviedb.org/3/movie/";
+let searchUrlBase = "https://api.themoviedb.org/3/search/";
 
 
-         let elementCont = crEl("div");
-         elementCont.className = "element-container";
-         let imgWrap = crEl("div");
-         imgWrap.className ="img-wrap";
-         let poster = crEl("img");
-         poster.src = imageServer+item.poster_path;
-         imgWrap.appendChild(poster);
-         elementCont.appendChild(imgWrap);
+getTopRated();
+
+function getTopRated(){
+    let url =`${urlBase}top_rated?api_key=${apikey}&language=${language}&page=${page}`;
+    getData(url)
+}
+function getLatest() {
+    let url =`${urlBase}now_playing?api_key=${apikey}&language=${language}`;
+    getData(url)
+}
+function getPopular() {
+    let url =`${urlBase}popular?api_key=${apikey}&language=${language}&page=${page}`;
+    getData(url)
+}
+function getUpcoming(){
+    let url =`${urlBase}upcoming?api_key=${apikey}&language=${language}&page=${page}`;
+    getData(url)
+}
 
 
-         let textContainer = crEl("div");
-         textContainer.className = "text-wrap";
+function searchMovie(keyword){
+    let url =`${searchUrlBase}movie?api_key=${apikey}&query=${keyword}&page=${page}`;
+    getData(url)
+}
+let search = getE("search");
 
-         let titleElement = crEl("p");
-         let titleHeadingWrap = crEl("span");
-         let titleHeading = crTe("Title: ");
-         titleHeadingWrap.appendChild(titleHeading);
-         let titleValue = crTe(item.title);
-         titleElement.appendChild(titleHeadingWrap);
-         titleElement.appendChild(titleValue);
-
-         let overviewElement = crEl("p");
-         let overviewHeadingWrap = crEl("span");
-         let overviewHeading = crTe("Description: ");
-         overviewHeadingWrap.appendChild(overviewHeading);
-         let overview = crTe(item.overview);
-         overviewElement.appendChild(overviewHeadingWrap);
-         overviewElement.appendChild(overview);
-
-         let voteElement = crEl("div");
-         let voteImg = crEl("img");
-         voteImg.className = "voteImg";
-         voteImg.src = "like.svg";
-         let voteHeading = crTe(item.vote_count);
-
-         voteElement.appendChild(voteImg);
-         voteElement.appendChild(voteHeading);
-
-
-         textContainer.appendChild(titleElement);
-         textContainer.appendChild(overviewElement);
-         textContainer.appendChild(voteElement);
-         elementCont.appendChild(textContainer);
-         container.appendChild(elementCont);
-     })
-    }
+search.onblur = function () {
+    sendKeyWord()
 };
-xhttp.open("GET", url, true);
-xhttp.send();
 
+search.onkeydown =function(event){
+if(event.which ===13){
+    sendKeyWord(this.value);
+}};
+
+function sendKeyWord(key){
+    let keyword = key;
+    if(keyword !==""){
+        searchMovie(keyword);
+    }
+}
+
+
+
+getE("latest").onclick = function () {
+    getLatest();
+};
+getE("toprated").onclick = function () {
+    getTopRated();
+};
+
+getE("popular").onclick = function () {
+    getPopular();
+};
+
+getE("upcoming").onclick = function () {
+    getUpcoming();
+};
+
+
+
+
+function getData(url){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let response = JSON.parse(xhttp.response);
+            let filmsArray =  response.results;
+            let container = getE("main");
+            container.innerHTML = "";
+            filmsArray.forEach(function (item) {
+                createMovieCard(item, container);
+            })
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+
+
+
+function createMovieCard(item, container) {
+
+
+
+    let elementCont = crEl("div");
+    elementCont.className = "element-container";
+    let imgWrap = crEl("div");
+    imgWrap.className ="img-wrap";
+    let poster = crEl("img");
+    poster.src = imageServer+item.poster_path;
+    imgWrap.appendChild(poster);
+    elementCont.appendChild(imgWrap);
+
+
+    let textContainer = crEl("div");
+    textContainer.className = "text-wrap";
+
+    let titleElement = crEl("p");
+    let titleHeadingWrap = crEl("span");
+    let titleHeading = crTe("Title: ");
+    titleHeadingWrap.appendChild(titleHeading);
+    let titleValue = crTe(item.title);
+    titleElement.appendChild(titleHeadingWrap);
+    titleElement.appendChild(titleValue);
+
+    let overviewElement = crEl("p");
+    let overviewHeadingWrap = crEl("span");
+    let overviewHeading = crTe("Description: ");
+    overviewHeadingWrap.appendChild(overviewHeading);
+    let overview = crTe(item.overview);
+    overviewElement.appendChild(overviewHeadingWrap);
+    overviewElement.appendChild(overview);
+
+    let voteElement = crEl("div");
+    let voteImg = crEl("img");
+    voteImg.className = "voteImg";
+    voteImg.src = "like.svg";
+    let voteHeading = crTe(item.vote_count);
+
+    voteElement.appendChild(voteImg);
+    voteElement.appendChild(voteHeading);
+
+
+    textContainer.appendChild(titleElement);
+    textContainer.appendChild(overviewElement);
+    textContainer.appendChild(voteElement);
+    elementCont.appendChild(textContainer);
+    container.appendChild(elementCont);
+}
 
 function crEl(element) {
 return document.createElement(element);
